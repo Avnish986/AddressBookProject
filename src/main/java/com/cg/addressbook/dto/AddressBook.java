9439c95161cc14534b1ac793cb9b027d77953bcc
avnish986 <avnishgupta986@gmail.com>
2020-10-02 17:30:52 +0530

    [Avnish]Updated: uc 5 multiple people

--- src/main/java/com/cg/addressbook/dto/AddressBook.java
+++ src/main/java/com/cg/addressbook/dto/AddressBook.java
@@ -3,11 +3,15 @@ import java.util.*;
 
 public class AddressBook {
 	private List<PersonContact> personContacts = new ArrayList<PersonContact>();
-	
+	String name;
 	
 	public List<PersonContact> getAddressBook(){
 		 return this.personContacts;
 	 }
+	public AddressBook(String name) {
+		this.name = name;
+	}
+	
 	public void addContact(PersonContact contactObj) {
 		personContacts.add(contactObj);
 	 }
@@ -19,6 +23,13 @@ public class AddressBook {
 			 }
 		}
 		return null;
+	}
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
 	}
 	public void  deletePerson(String firstName) {
 		for(int i=0 ; i<personContacts.size();i++) {
--- src/main/java/com/cg/addressbook/service/AddressBookService.java
+++ src/main/java/com/cg/addressbook/service/AddressBookService.java
@@ -6,5 +6,6 @@ public interface AddressBookService {
 	public void showOptions(AddressBook addressBook);
 	public void updateAPerson();
 	public void deleteAPerson();
+	public AddressBook createAddressBook(String name);
 
 }
